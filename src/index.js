83527720146fac3017594d7d8d804d5eaa43b362
pprathamesh <prathameshp@axioned.com>
2021-12-22 15:20:10 +0530

    feat: delete unused file/folder from source folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+import ReactDOM from 'react-dom'; 
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider>
+      <h1>Hello React</h1>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
